9dc98e21a2a2bb552294e75a2389e643c2f302f4
Louise <louisegabrielvilar69@gmail.com>
2024-12-26 22:44:52 +0800

    make-operation:

--- skip-list/dynamic_list.h
+++ skip-list/dynamic_list.h
@@ -9,24 +9,24 @@
 /*
 what we actually need for the a skip node are: next, val, level
 */
-typedef struct skip_node{
-    int val; // integer value of a node
-    struct skip_node *left; // link to the left adjacent node
-    struct skip_node *right; // link to the right adjacent node
-    struct skip_node *below; // link to the down level node
+typedef struct skip_node {
+    int level; // current level based on flipped coin
+    int key; // acts as the index of the node
+    int val;  // integer value of a node
+    struct skip_node *right;  // link to the right adjacent nodes
+    struct skip_node *left; // link to the left adjacent nodes
+    struct skip_node *below; // link to the each levels; level 0, node is linked to NULL
 } node;
-/*
-what we actually need for the a skip list are: height, length and head
-*/
-typedef struct skip_list{
-    int length; // horizontal size of the skip_list 
-    int height; // vertical size of the skip_list 
-    struct skip_node *head; // points to the head of each list
+
+typedef struct skip_list {
+    int length; // maximum horizontal size
+    int height; // maximum vertical size
+    node *header; // sentinel head value; initialized to INT_MIN
+    node *footer;// sentinel tail value; initialized to INT_MAX
     bool is_reversed;
 } list;
 
 
-list init(void);
 list *make(int n, int64_t *seq);
 void push_left(list *l, int64_t v);
 void push_right(list *l, int64_t v);
