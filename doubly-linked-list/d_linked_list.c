dca5f3418febab20848797ba187effe9a8727b33
Louise <louisegabrielvilar69@gmail.com>
2024-12-28 17:27:16 +0800

    AC:

--- doubly-linked-list/d_linked_list.c
+++ doubly-linked-list/d_linked_list.c
@@ -3,10 +3,15 @@
 #include <stdlib.h>
 #include "d_linked_list.h"
 
-// Following are the OPERATIONS for the implementation
+// --------------------------------------------------------- >>
+/* ----------------------------------------- <<
 
-//*-------------------------------------------------------------*//
+            ||-- MAIN OPERATIONS --||
 
+<< ----------------------------------------- */
+// --------------------------------------------------------- >>
+
+// Init 
 list *init(){
     list *l = malloc(sizeof(list));
     l->head = l->tail =  NULL;
@@ -23,6 +28,7 @@ list *make(LENGTH n, DATA *seq){
     list *ptr = init();
     node *curr = head;
 
+    // if empty, return an empty list
     if (n == 0){
         curr->val = 0;
         curr->left = curr->right = NULL;
@@ -30,12 +36,14 @@ list *make(LENGTH n, DATA *seq){
         ptr->size = 0;
         return ptr;
     }
+
     ptr->head = head;
     ptr->size = n;
     curr->right = NULL;
 
-    for (LENGTH i = 0; i <= n; i++){
-        // if the index = 0, means that it is the first element
+    // traverse through the sequence
+    for (LENGTH i = 0; i < n; i++){
+        // get the first element
         if (i == 0){
             curr->val = seq[i];
             curr->left = NULL;
@@ -54,129 +62,9 @@ list *make(LENGTH n, DATA *seq){
     return ptr;
 }
 
-void push_left(list *l, DATA v){
-    node *newNode = (node*)malloc(sizeof(node));
-    newNode->val = v;
-    newNode->left = newNode->right = NULL;
-    node *curr = l->reversed ? l->head : l->tail;
-    //empty_list case
-    if (!(curr)) l->head = l->tail = newNode;
-    else {
-        if (!(l->reversed)){
-            newNode->right = l->head;
-            l->head->left = newNode;
-            l->head = newNode;
-        }
-        else {
-            newNode->left = l->tail;
-            l->tail->right = newNode;
-            l->tail = newNode;
-        }
-    }
-    (l->size)++;
-}
-
-void push_right(list *l, DATA v){
-    node *newNode = (node*)malloc(sizeof(node));
-    newNode->val = v;
-    newNode->left = newNode->right = NULL;
-    node *curr = l->reversed ? l->head : l->tail;
-    //empty_list case
-    if (!(curr)) l->head = l->tail = newNode;
-    else {
-        if (l->reversed){
-            newNode->right = l->head;
-            l->head->left = newNode;
-            l->head = newNode;
-        }
-        else {
-            newNode->left = l->tail;
-            l->tail->right = newNode;
-            l->tail = newNode;
-        }
-    }
-    (l->size)++;
-}
-
-bool pop_left(list *l){
-    // empty_list
-    if (!(l->head) && !(l->tail)){
-        fprintf(stderr, "\nIndexError: Pop From Empty List\n");
-        return false;
-    };
-    node *curr = !(l->reversed) ? l->head : l->tail;
-    node *store = !(l->reversed) ? curr->right : curr->left; // if left, then store next
-    // single_node case
-    if ((l->tail) == (l->head)){
-        l->head = l->tail = NULL;
-        free(curr);
-        // reduce the size
-        (l->size)--;
-        // free the node
-        return true; 
-    }
-    else {
-        if (!(l->reversed)){ // reversed toggled ON
-            store->left = NULL;
-            l->head = store;
-        }
-        else {
-            store->right = NULL;
-            l->tail = store;
-        }
-    }
-    (l->size)--;
-    free(curr);
-    return true;
-}
-
-bool pop_right(list *l){
-    // empty_list
-    if (!(l->head) && !(l->tail)) return false;
-    node *curr = l->reversed ? l->head : l->tail;
-    node *store = l->reversed ? curr->right : curr->left; // if right, then store prev
-    // single_node case
-    if ((l->tail) == (l->head)){
-        l->head = l->tail = NULL;
-        free(curr);
-        // reduce the size
-        (l->size)--;
-        // free the node
-        return true; 
-    }
-    else {
-        if (l->reversed){ // reversed toggled ON
-            store->left = NULL;
-            l->head = store;
-        }
-        else {
-            store->right = NULL;
-            l->tail = store;
-        }
-    }
-    (l->size)--;
-    free(curr);
-    return true;
-}
-
-DATA peek_left(list *l){
-    // peek from an empty list
-    if (!(l->head) && !(l->tail)){
-        fprintf(stderr, "\nIndexError: Peek From Empty List\n");
-        exit(1);\
-    }
-    node *curr = !(l->reversed) ? l->head : l->tail;
-    return curr->val;
-}
-
-DATA peek_right(list *l){
-    // peek from an empty list
-    if (!(l->head) && !(l->tail)){
-        fprintf(stderr, "\nIndexError: Peek From Empty List\n");
-        exit(1);\
-    }
-    node *curr = (l->reversed) ? l->head : l->tail;
-    return curr->val;
+// Flags/List Info
+void reverse(list *l){
+    l->reversed = !(l->reversed);
 }
 
 LENGTH size(list *l){
@@ -191,6 +79,7 @@ bool empty(list *l){
     return (l->size == 0);
 }
 
+// Getters/Setters
 DATA get(list *l, LENGTH i){
     node *curr = l->reversed ? l->tail : l->head;
     // raise an exception
@@ -239,19 +128,146 @@ void set(list *l, LENGTH i, DATA v){
 
 }
 
-void reverse(list *l){
-    // l->reversed = l->reversed == 0 ? 1 : 0;
-    // l->reversed = !(l->reversed);
-    if (l->reversed == 0){
-        l->reversed = 1;
+DATA peek_left(list *l){
+    // empty-list
+    if (!(l->head) && !(l->tail)){
+        fprintf(stderr, "\nIndexError: Peek From Empty List\n");
+        exit(1);\
     }
-    else {
-        l->reversed = 0;
-    }
+    node *curr = !(l->reversed) ? l->head : l->tail;
+    return curr->val;
 }
 
+DATA peek_right(list *l){
+    // empty-list
+    if (!(l->head) && !(l->tail)){
+        fprintf(stderr, "\nIndexError: Peek From Empty List\n");
+        exit(1);\
+    }
+    node *curr = (l->reversed) ? l->head : l->tail;
+    return curr->val;
+}
+
+// Insertions/Deletions
+void push_left(list *l, DATA v){
+    node *newNode = (node*)malloc(sizeof(node));
+    newNode->val = v;
+    newNode->left = newNode->right = NULL;
+    node *curr = l->reversed ? l->head : l->tail;
+
+    // empty-list case
+    if (!(curr)) l->head = l->tail = newNode;
+
+    else {
+        if (!(l->reversed)){
+            newNode->right = l->head;
+            l->head->left = newNode;
+            l->head = newNode;
+        }
+        else {
+            newNode->left = l->tail;
+            l->tail->right = newNode;
+            l->tail = newNode;
+        }
+    }
+    (l->size)++;
+}
+
+void push_right(list *l, DATA v){
+    node *newNode = (node*)malloc(sizeof(node));
+    newNode->val = v;
+    newNode->left = newNode->right = NULL;
+    node *curr = l->reversed ? l->head : l->tail;
+
+    //empty_list case
+
+    if (!(curr)) l->head = l->tail = newNode;
+    else {
+        if (l->reversed){
+            newNode->right = l->head;
+            l->head->left = newNode;
+            l->head = newNode;
+        }
+        else {
+            newNode->left = l->tail;
+            l->tail->right = newNode;
+            l->tail = newNode;
+        }
+    }
+    (l->size)++;
+}
+
+bool pop_left(list *l){
+    // empty-list
+    if (!(l->head) && !(l->tail)){
+        fprintf(stderr, "\nIndexError: Pop From Empty List\n");
+        return false;
+    };
+
+    node *curr = !(l->reversed) ? l->head : l->tail;
+    node *store = !(l->reversed) ? curr->right : curr->left; // if left, then store next
+
+    // single-element case
+    if ((l->tail) == (l->head)){
+        l->head = l->tail = NULL;
+        // free the node
+        free(curr);
+        // reduce the size
+        (l->size)--;
+        return true; 
+    }
+    else {
+        if (!(l->reversed)){ // reversed toggled ON
+            store->left = NULL;
+            l->head = store;
+        }
+        else {
+            store->right = NULL;
+            l->tail = store;
+        }
+    }
+    (l->size)--;
+    free(curr);
+    return true;
+}
+
+bool pop_right(list *l){
+    // empty_list
+    if (!(l->head) && !(l->tail)){
+        fprintf(stderr, "\nIndexError: Pop From Empty List\n");
+        return false;
+    };
+
+    node *curr = l->reversed ? l->head : l->tail;
+    node *store = l->reversed ? curr->right : curr->left; // if right, then store prev
+
+    // single-element case
+    if ((l->tail) == (l->head)){
+        l->head = l->tail = NULL;
+        // free the node
+        free(curr);
+        // reduce the size
+        (l->size)--;
+        return true; 
+    }
+    else {
+        if (l->reversed){ // reversed toggled ON
+            store->left = NULL;
+            l->head = store;
+        }
+        else {
+            store->right = NULL;
+            l->tail = store;
+        }
+    }
+    (l->size)--;
+    free(curr);
+    return true;
+}
+
+// Print
 void display(list *l){
-    // if (l->size == 0) return;
+    if (l->size == 0) return;
     node *curr = l->head;
     printf("\n");
     while (curr){
@@ -260,13 +276,6 @@ void display(list *l){
     }
     printf("NULL \n");
 }
-
-// DATA *make_seq(LENGTH size, DATA *array){
-//     for (LENGTH i = 0; i < size; i++){
-//         array[i] = i;
-//     }
-//     return array;
-// }
 
 
 // --------------------------------------------------------- >>
@@ -283,7 +292,7 @@ void TEST_elements(list* l, LENGTH* nRef, DATA** seqRef){
     list *dl = l;
     DATA *seq = malloc((dl->size) * sizeof(DATA*));
     node *curr = dl->head;
-    while (curr->right){
+    while (curr){
         seq[i] = curr->val;
         curr = curr->right;
         i++;
@@ -296,20 +305,8 @@ bool TEST_internal(list* l){
     return true;
 }
 
-DATA *make_seq(LENGTH size){
-    DATA *array = malloc(size * sizeof(DATA));
-    for (LENGTH i = 0; i < size; i++){
-        array[i] = i;
-    }
-    return array;
-}
-
-LENGTH main(){
-    LENGTH n = 5;
-    DATA *array = make_seq(n);
-    DATA *seq = malloc(n * sizeof(DATA*));
-    list *l = make(n, array);
-    TEST_elements(l,&n,&seq);
+bool TEST_reversed(list* l){
+  return (l->reversed);
 }
 
 
