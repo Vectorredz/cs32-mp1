d3a9daf7e066c79881d03596d8216cc1df4bbc85
Louise <louisegabrielvilar69@gmail.com>
2024-12-29 18:54:02 +0800

    :

--- skip-list/main3.c
+++ skip-list/main3.c
@@ -5,9 +5,12 @@
 #include <time.h>
 #include "skip-list.h"
 
+#define UPPERBOUND 64
+
+
 // initialize the nodes
 node *makeNode(LENGTH key){
-    node *newNode = malloc(sizeof(node));
+    node *newNode = (node*)malloc(sizeof(node));
     newNode->below = newNode->right = newNode->left = NULL; // initialize the pointers to NULL
     newNode->key = key; // set the key 
     return newNode; // return the created node
@@ -16,19 +19,29 @@ node *makeNode(LENGTH key){
 // initialize the skip-list
 list *initSkipList(){
     list *skip_list = malloc(sizeof(list));
-    node *sentinel_left = skip_list->leftmost = INT_MIN; // initially set the leftmost to the sentinel 
-    node *sentinel_right = skip_list->rightmost = INT_MAX; // initially set the righmost to the sentinel 
-    
-    // link the sentinels 
-    sentinel_left->right = sentinel_right;
-    sentinel_right->left = sentinel_left;
 
+    skip_list->leftmost = skip_list->rightmost = NULL;
+
+    // initialize the sentinels
+    node *front = skip_list->front = malloc(sizeof(node));
+    // node *rear = skip_list->rear = malloc(sizeof(node));
+    front->key = INT_MIN;
+    front->left = NULL;
+    // rear->key = INT_MAX;
+    // rear->right = NULL;
+
+    // link the sentinels 
+    front->right = NULL;
+    front->below = NULL;
+ 
     skip_list->is_reversed = false; // default : toggled off
     skip_list->maxHeight = 0; 
     skip_list->size = 0;
+
+    return skip_list;
 }
 
-// HELPER
+// HELPERS
 
 // Search
 node *search(list *sl, LENGTH key){
@@ -36,27 +49,115 @@ node *search(list *sl, LENGTH key){
     node *header = sl->leftmost;
     
     // traverse through the header until key found
-    while (header->below && key > header->key){ // when current traversal is not NULL 
-        // seearch one level down
-        header = header->below;
+    while (header && key > header->key){ // when current traversal is not NULL 
         // search one level right 
-        while (header->right->key < key){
+        while (header->key < key){
             header = header->right;
         }
         // if key is found 
         if (key == header->key){
             return header; // return the node
         }
+        // search one level down
+        header = header->below;
     }
-    return header;
+    return NULL; // key is not found
 }
 
-node *insert(int key){
+node *searchBottomMost(list *sl, LENGTH key, node **seqRef, LENGTH *sizeRef){
+    node *header = sl->front; // topmost sentinel node
+    *sizeRef = 0;
+    while (header){
+        while(header->right){
+            header = header->right;
+        }
+        // track each levels and store the nodes
+        seqRef[(*sizeRef)++] = header;
+        header = header->below;
+    }
     
 }
 
+// bool promoteLevel(){
+//     return (rand() < (RAND_MAX / 2));
+// }
 
+void push_left(list *l, DATA v) {
+    node *seqRef[64];
+    LENGTH sizeRef = 0;
+    searchBottomMost(l, v, seqRef, &sizeRef);
+
+    node *belowNode = NULL;
+    bool promote = true;
+
+    while (promote && sizeRef > 0) {
+        node *sentinel = seqRef[--sizeRef];
+        node *newNode = makeNode(v);
+
+        // Link horizontally
+        newNode->right = sentinel->right;
+        if (sentinel->right) {
+            sentinel->right->left = newNode;
+        }
+        sentinel->right = newNode;
+        newNode->left = sentinel;
+
+        // Link vertically
+        newNode->below = belowNode;
+        belowNode = newNode;
+
+        // Update leftmost if necessary
+        if (l->leftmost == NULL || v < l->leftmost->key) {
+            l->leftmost = newNode;
+        }
+
+        promote = ((rand() % 2) == 0);
+    }
+
+    // Add new level if needed
+    if (promote) {
+        node *newFront = malloc(sizeof(node));
+        newFront->key = INT_MIN;
+        node *newNode = makeNode(v);
+
+        newFront->below = l->front;
+        newFront->right = newNode;
+        newFront->left = NULL;
+
+        newNode->left = newFront;
+        newNode->right = NULL;
+        newNode->below = belowNode;
+
+        l->front = newFront;
+        l->maxHeight++;
+    }
+
+    l->size++;
+}
+
+
+void display(list* skiplist) {
+    node* level = skiplist->front;
+    while (level) {
+        node* temp = level;
+        while (temp) {
+            printf("%d -> ", temp->key);
+            temp = temp->right;
+        }
+        printf("\n");
+        level = level->below;
+    }
+}
 
 LENGTH main(){
+    srand(time(NULL));
+
+    // Initialize the skip list
+    list *sl = initSkipList();
+
+    push_left(sl, 0);
+    push_left(sl, 1);
+    // push_left(sl, 3);
+    display(sl);
 
 }
--- skip-list/skip-list.h
+++ skip-list/skip-list.h
@@ -1,63 +1,71 @@
 #ifndef SKIP_LIST_H
 #define SKIP_LIST_H
-#include <../H_global.h>
+#include "../H_global.h"
 #include <stdbool.h>
 #include <stdint.h>
 
-// ADT for a skip list node
-// **** Assuming that the lanes: express and normal are sorted and is doubly-linked list ***** //
+typedef struct _SkipNode {
+    struct _SkipNode *right;  // link to the right adjacent nodes
+    struct _SkipNode *left; // link to the left adjacent nodes
+    struct _SkipNode *below; // link to the each levels; level 0, node is linked to NULL
+    bool isSentinel; // flag
+    DATA val;  
+    LENGTH width;
+} SkipNode;
 
-/*
-what we actually need for the a skip node are: next, val, level
-*/
-typedef struct skip_node {
-    struct skip_node *right;  // link to the right adjacent nodes
-    struct skip_node *left; // link to the left adjacent nodes
-    struct skip_node *below; // link to the each levels; level 0, node is linked to NULL
-    // LENGTH level; // current level based on flipped coin
-    LENGTH key; // acts as the index of the node
-    // LENGTH val;  // integer value of a node
-} node;
+typedef struct _Level {
+    struct _Level *up;
+    struct _Level *down;
+    SkipNode *sentinel; // head
+    SkipNode *tail; // tail
+} Level;
 
-typedef struct skip_list {
-    node *leftmost; // sentinel : INT_MIN
-    node *rightmost; // seintinel : INT_MAX
-    LENGTH maxHeight; 
+typedef struct _LevelList {
+    Level *top; // header
+    Level *bottom; // level 0 zero
+} LevelList;
+
+typedef struct _SkipList {
+    LevelList *levels;
+    
+    DATA leftmost;
+    DATA rightmost;
+    
+    LENGTH maxHeight;
+    LENGTH currHeight;
     LENGTH size;
+    
     bool is_reversed;
-} list;
+} SkipList;
 
-typedef struct doubly_linked_list { // tailRecord
-    node **record;
-    struct doubly_linked_list *prev; 
-    struct doubly_linked_list *next;
-} dlist; 
-
+// HELPERS
+SkipNode *_getNode(SkipList *l, LENGTH i);
 
 // Init
-list *init();
-list *make(LENGTH n, DATA *seq);
+SkipList *_initSkipList();
+SkipList *make(LENGTH n, DATA *seq);
 
-// Flags/List Info
-void reverse(list *l);
-LENGTH size(list *l);
-bool empty(list *l);
+// Flags/SkipList Info
+void reverse(SkipList *l);
+LENGTH size(SkipList *l);
+bool empty(SkipList *l);
 
 // Getters/Setters
-DATA get(list *l, LENGTH i);
-void set(list *l, LENGTH i, DATA v);
-DATA peek_left(list *l);
-DATA peek_right(list *l);
+DATA get(SkipList *l, LENGTH i);
+void set(SkipList *l, LENGTH i, DATA v);
+DATA peek_left(SkipList *l);
+DATA peek_right(SkipList *l);
 
 // Insertions/Deletions
-void push_left(list *l, DATA v);
-void push_right(list *l, DATA v);
-bool pop_left(list *l);
-bool pop_right(list *l);
+void push_left(SkipList *l, DATA v);
+void push_right(SkipList *l, DATA v);
+bool pop_left(SkipList *l);
+bool pop_right(SkipList *l);
 
 // Test Operations
-void TEST_elements(list* l, LENGTH* nRef, DATA** seqRef);
-bool TEST_internal(list* l);
+void TEST_elements(SkipList* l, LENGTH* nRef, DATA** seqRef);
+bool TEST_internal(SkipList* l);
+bool TEST_reversed(SkipList* l);
 
 
 #endif
