bd3e835b58362a088fe536d35aee21d76cb4e5c5
Louise <louisegabrielvilar69@gmail.com>
2025-01-02 04:46:58 +0800

    update :

--- mirror-flower/test_settings_m.py
+++ mirror-flower/test_settings_m.py
@@ -12,7 +12,7 @@ INPUT_DIRECTORY = Path("inputs")
 # This will output a large number of outputs to the SPECIAL layer .txt file in INPUT_DIRECTORY, for graph plotting.
 # For efficiency test ONLY (The specific test lines with large inputs do NOT check for correctness)
 # (WARNING: takes a long time to generate/test)
-LARGE_INPUTS = False
+LARGE_INPUTS = True
 
 # The randomizer seed. ("None" if random)
 SEED = None
